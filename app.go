8c6cef77dab37aae772fe8646c2f14481d2f260c
Mark Bates <mark@markbates.com>
2018-10-18 15:28:20 -0400

    cleaned up toml

--- app.go
+++ app.go
@@ -23,23 +23,23 @@ type App struct {
 	Pwd         string     `json:"pwd" toml:"-"`
 	Root        string     `json:"root" toml:"-"`
 	GoPath      string     `json:"go_path" toml:"-"`
-	Name        name.Ident `json:"name"`
-	Bin         string     `json:"bin"`
-	PackagePkg  string     `json:"package_path"`
-	ActionsPkg  string     `json:"actions_path"`
-	ModelsPkg   string     `json:"models_path"`
-	GriftsPkg   string     `json:"grifts_path"`
-	VCS         string     `json:"vcs"`
-	WithPop     bool       `json:"with_pop"`
-	WithSQLite  bool       `json:"with_sqlite"`
-	WithDep     bool       `json:"with_dep"`
-	WithWebpack bool       `json:"with_webpack"`
-	WithYarn    bool       `json:"with_yarn"`
-	WithDocker  bool       `json:"with_docker"`
-	WithGrifts  bool       `json:"with_grifts"`
+	PackagePkg  string     `json:"package_path" toml:"-"`
+	ActionsPkg  string     `json:"actions_path" toml:"-"`
+	ModelsPkg   string     `json:"models_path" toml:"-"`
+	GriftsPkg   string     `json:"grifts_path" toml:"-"`
 	WithModules bool       `json:"with_modules" toml:"-"`
-	AsWeb       bool       `json:"as_web"`
-	AsAPI       bool       `json:"as_api"`
+	Name        name.Ident `json:"name" toml:"name"`
+	Bin         string     `json:"bin" toml:"bin"`
+	VCS         string     `json:"vcs" toml:"vcs"`
+	WithPop     bool       `json:"with_pop" toml:"with_pop"`
+	WithSQLite  bool       `json:"with_sqlite" toml:"with_sqlite"`
+	WithDep     bool       `json:"with_dep" toml:"with_dep"`
+	WithWebpack bool       `json:"with_webpack" toml:"with_webpack"`
+	WithYarn    bool       `json:"with_yarn" toml:"with_yarn"`
+	WithDocker  bool       `json:"with_docker" toml:"with_docker"`
+	WithGrifts  bool       `json:"with_grifts" toml:"with_grifts"`
+	AsWeb       bool       `json:"as_web" toml:"as_web"`
+	AsAPI       bool       `json:"as_api" toml:"as_api"`
 }
 
 func (a App) IsZero() bool {
